30d41fddf2cca0c0879fa1a12e47675b62743d4a
Ivan Shnurchenko <ivan.shnurchenko@gmail.com>
2019-12-03 14:58:10 +0300

    Added GraphQL errors

---
+++ gql/errors/error.go
@@ -0,0 +1,46 @@
+package errors
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type ApiError struct {
+	err        error
+	ErrCode    string
+	ErrMessage string
+	HTTPCode   int
+}
+
+func (ae ApiError) Error() string {
+	var err string
+	if ae.err != nil {
+		err = fmt.Sprintf("%s: %s", ae.ErrMessage, fmt.Errorf("%w", ae.err).Error())
+	} else {
+		err = fmt.Sprintf("%s", ae.ErrMessage)
+	}
+
+	return err
+}
+
+func (ae *ApiError) Unwrap() error {
+	return ae.err
+}
+
+func NewApiError(err error, code string, msg string, httpCode int) ApiError {
+	return ApiError{
+		err:        err,
+		ErrCode:    code,
+		ErrMessage: msg,
+		HTTPCode:   httpCode,
+	}
+}
+
+func NewUnauthorizedError(err error) ApiError {
+	return ApiError{
+		err:        err,
+		ErrCode:    "unauthorized",
+		ErrMessage: "Unauthorized",
+		HTTPCode:   http.StatusUnauthorized,
+	}
+}
