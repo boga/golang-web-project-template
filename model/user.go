1797014eab49941d7d70ab6471e870a71bcf226a
Ivan Shnurchenko <ivan.shnurchenko@gmail.com>
2019-12-03 14:52:17 +0300

    Added DB struct tags for User model

--- model/user.go
+++ model/user.go
@@ -1,9 +1,12 @@
 package model
 
 type User struct {
-	ID             int
-	Name           *string
-	AuthIdentities []AuthIdentity
+	ID              int     `db:"id" json:"id"`
+	Name            *string `db:"name" json:"name"`
+	AuthIdentities  []AuthIdentity
+	TOTPBackupCodes *string `db:"totp_backup_codes" json:"totp_backup_codes"`
+	TOTPEnabled     bool    `db:"totp_enabled" json:"totp_enabled"`
+	TOTPSecret      *string `db:"totp_secret" json:"totp_secret"`
 }
 
 type AuthIdentity struct {
